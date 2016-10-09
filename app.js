004db1c48bbad6318de9c3a044b5300e93d5b56f
Mohammad Nassar <safeermohammed13190@yahoo.co.uk>
2016-10-09 14:53:36 +0100

    Iterating through a list/collection, adding a function to the $scope and reordering the list.

--- app.js
+++ app.js
@@ -3,5 +3,15 @@ var app = angular.module('flapperNews', []);
 app.controller('MainCtrl', [
 	'$scope', 
 	function($scope) {
-		$scope.test = 'Hello World';
+		$scope.posts = [
+			{title: 'Post 1', upvotes: 5}, 
+			{title: 'Post 2', upvotes: 3}, 
+			{title: 'Post 3', upvotes: 7}, 
+			{title: 'Post 4', upvotes: 6}, 
+			{title: 'Post 5', upvotes: 4}, 
+		];
+		
+		$scope.addPost = function() {
+			$scope.posts.push({title: 'New post', upvotes: 0});
+		};
 }]);
