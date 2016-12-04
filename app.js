3d50ecf46bb924269aa415e93f8c8fcd10af22ee
Mohammad Nassar <safeermohammed13190@yahoo.co.uk>
2016-12-04 11:07:23 +0000

    - Performing Angular routing. - Dealing with multiple views and controllers, using the 'ui-router' library. - Configuring the 'ui-router' by using Angular's 'config()' function to setup a home state. - Also defining the 'home.html' into an inline template, instead of creating a new file. HENCE: The 'ng-controller' attribute in the <body> tag in 'index.html' file should be removed as it is now added in 'app.config()' function in 'app.js' file, however, when this is done  the app stops working!!

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
 
 app.factory('posts', [function(){
 	var obj = {
@@ -28,3 +28,19 @@ app.controller('MainCtrl', [
 			post.upvotes += 1;
 		};
 }]);
+
+app.config([
+	'$stateProvider', 
+	'$urlRouterProvider', 
+	function($stateProvider, $urlRouterProvider){
+		
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl'
+		});
+	
+	$urlRouterProvider.otherwise('home');
+	}
+]);
